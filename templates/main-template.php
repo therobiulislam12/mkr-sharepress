ce1b621dc143f86a70188c94be81200e33d742ee
Robiul Islam <therobiulislam12@gmail.com>
2024-10-27 02:39:00 +0600

    Single Template added

---
+++ templates/main-template.php
@@ -0,0 +1,26 @@
+<section class="ssp-template-main-template">
+    <div class="ssp-template-container">
+        <div class="ssp-template-icons">
+            <a href="https://www.facebook.com/sharer/sharer.php?u=https://yourwebsite.com" target="_blank"
+                onclick="return openPopup(this.href)">
+                <img src="./images/facebook.png" alt="Share on Facebook">
+            </a>
+
+            <a href="https://twitter.com/intent/tweet?url=https://yourwebsite.com&text=Check this out!"
+                target="_blank" onclick="return openPopup(this.href)">
+                <img src="./images/twitter.png" alt="Share on Twitter">
+            </a>
+
+            <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://yourwebsite.com" target="_blank"
+                onclick="return openPopup(this.href)">
+                <img src="./images/linkedin.png" alt="Share on LinkedIn">
+            </a>
+
+            <a href="https://api.whatsapp.com/send?text=https://yourwebsite.com" target="_blank"
+                onclick="return openPopup(this.href)">
+                <img src="./images/whatsapp.png" alt="Share on WhatsApp">
+            </a>
+        </div>
+
+    </div>
+</section>
