fb4b2d7e1983fb02bf907839702dd0a086991538
Robiul Islam <therobiulislam12@gmail.com>
2024-10-29 04:40:57 +0600

    Solve Plugin check error

--- templates/main-template.php
+++ templates/main-template.php
@@ -15,25 +15,25 @@ $permitted_links = get_option( 'sspp_show_social_links', [] );
                     <?php if ( in_array( 'facebook', $permitted_links ) ) {?>
                     <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo esc_url( get_the_permalink() ) ?>" target="_blank"
                     onclick="return openPopup(this.href)">
-                        <img src="<?php echo SSPP_PLUGIN_URI . '/assets/images/facebook.png' ?>" alt="Share on Facebook">
+                        <img src="<?php echo esc_url(SSPP_PLUGIN_URI . '/assets/images/facebook.png') ?>" alt="Share on Facebook">
                     </a>
                 <?php }
                 if ( in_array( 'twitter', $permitted_links ) ) {?>
                     <a href="https://twitter.com/intent/tweet?url=<?php echo esc_url( get_the_permalink() ) ?>&text=Check this out!"
                     target="_blank" onclick="return openPopup(this.href)">
-                        <img src="<?php echo SSPP_PLUGIN_URI . '/assets/images/twitter.png' ?>" alt="Share on Twitter">
+                        <img src="<?php echo esc_url(SSPP_PLUGIN_URI . '/assets/images/twitter.png') ?>" alt="Share on Twitter">
                     </a>
                 <?php }
                     if ( in_array( 'linkedin', $permitted_links ) ) {?>
                     <a href="https://www.linkedin.com/sharing/share-offsite/?url=<?php echo esc_url( get_the_permalink() ) ?>" target="_blank"
                     onclick="return openPopup(this.href)">
-                        <img src="<?php echo SSPP_PLUGIN_URI . '/assets/images/linkedin.png' ?>" alt="Share on LinkedIn">
+                        <img src="<?php echo esc_url(SSPP_PLUGIN_URI . '/assets/images/linkedin.png') ?>" alt="Share on LinkedIn">
                     </a>
                 <?php }
                     if ( in_array( 'whatsapp', $permitted_links ) ) {?>
                     <a href="https://api.whatsapp.com/send?text=<?php echo esc_url( get_the_permalink() ) ?>" target="_blank"
                     onclick="return openPopup(this.href)">
-                        <img src="<?php echo SSPP_PLUGIN_URI . '/assets/images/whatsapp.png' ?>" alt="Share on WhatsApp">
+                        <img src="<?php echo esc_url(SSPP_PLUGIN_URI . '/assets/images/whatsapp.png' ) ?>" alt="Share on WhatsApp">
                     </a>
                 <?php }?>
             </div>
